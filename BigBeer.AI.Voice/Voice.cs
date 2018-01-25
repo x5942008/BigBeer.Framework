81a6d0babe4a706532e8f19ba20b6d7230f42c36
某 吴 <549590615@qq.com>
2018-01-25 14:28:14 +0800

    AI语音

---
+++ BigBeer.AI.Voice/Voice.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Baidu.Aip.Speech;
+using Microsoft.Extensions.Configuration;
+
+namespace BigBeer.AI.Voice
+{
+    public class Voices
+    {
+        private static IConfigurationRoot build => new ConfigurationBuilder()
+        .AddJsonFile("appsetting.json", false, true)
+        .Build();
+
+        private static string apikey => build["Voice1:API_KEY"];
+        private static string secret_key => build["Voice1:SECRET_KEY"];
+        private static Asr asr => new Asr(apikey, secret_key);
+        private static Tts tts => new Tts(apikey, secret_key);
+
+        /// <summary>
+        /// 识别语音
+        /// </summary>
+        /// <param name="url">文件地址</param>
+        /// <param name="format">目前格式仅仅支持pcm，wav或amr，如填写mp3即会有此错误</param>
+        /// <returns></returns>
+        public static string AsrData(string url, string format)
+        {
+            var data = File.ReadAllBytes(url);
+            var result = asr.Recognize(data, format, 16000);
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// 语音合成
+        /// </summary>
+        /// <param name="content">合成内容</param>
+        /// <param name="path">存储地址</param>
+        /// <param name="suffix">目前格式仅仅支持pcm，wav或amr，如填写mp3即会有此错误</param>
+        /// <param name="speed">语速</param>
+        /// <param name="vol">声量</param>
+        /// <param name="per">0为女声，1为男声，3为情感合成男，4为情感女</param>
+        public static void Tts(string content, string path, string suffix = "wav", int per = 1, int speed = 5, int vol = 7)
+        {
+            string sf = "wav";
+            int p = 1;
+            int s = 5;
+            int v = 7;
+            if (per != 1) p = per;
+            if (speed != 5) s = speed;
+            if (per != 7) v = vol;
+
+            // 可选参数
+            var option = new Dictionary<string, object>()
+    {
+        {"spd", s}, // 语速
+        {"vol", v}, // 音量
+        {"per", p}  // 发音人，4：情感度丫丫童声
+    };
+            var result = tts.Synthesis(content, option);
+
+            if (!string.IsNullOrEmpty(suffix))
+                sf = suffix;
+            if (result.ErrorCode == 0)  // 或 result.Success
+            {
+                File.WriteAllBytes(path + $".{sf}", result.Data);
+            }
+        }
+    }
+}
--- BigBeer.Framework.Test/Program.cs
+++ BigBeer.Framework.Test/Program.cs
@@ -4,6 +4,7 @@ using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BigBeer.AI.Voice;
 using BigBeer.Framework.QR_Code;
 
 namespace BigBeer.Framework.Test
@@ -12,30 +13,39 @@ namespace BigBeer.Framework.Test
     {
         static void Main(string[] args)
         {
-            string path = @"F:\";
-            string url = "http://www.wujixiong.com";
-            string name1 = "wujixiong";
-            string name2 = "bigbeerqr";
-            string name3 = "bigbeerzx";
-            string BarCode = "20180123";//条形码数字（双数，80个以内）
-            ImgType type = ImgType.Png;
-            var sufiix = Suffix.Getsuffix(type);
-            string logo = @"F:\123.jpg";//Logo地址 需要修改
-            QrCode_QR.Generate(url,name1,path,type);//普通二维码
-            Console.WriteLine($"文件保存成功，请在路径{path}查看文件{name1}{sufiix}");
-            QRCode_Zxing.Generate(url,logo,path,name2,type,800,800);//带logo二维码 并且自定义大小
-            Console.WriteLine($"文件保存成功，请在路径{path}查看文件{name2}{sufiix}");
-            QRCode_Zxing.Generate2(BarCode,path,name3,type,300,100);//宽高比例3:1
-            Console.WriteLine($"文件保存成功，请在路径{path}查看文件{name3}{sufiix}");
-            //读取解析二维码和条形码
-            var result1 =  QRCode_Zxing.Read($"{path}{name1}{sufiix}");
-            var result2 = QRCode_Zxing.Read($"{path}{name2}{sufiix}");
-            var result3 = QRCode_Zxing.Read($"{path}{name3}{sufiix}");
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
-            Console.WriteLine(result3);
 
+            string content = "大扎好，我系轱天乐，我四脏渣辉，探挽懒月，介四里没有挽过的船新版本，挤需体验三番钟，里造会干我一样，爱象戒宽油系";
+            string path = @"E:\Voice\";
+            string filename = "tts2";
+            string type = "mp3";
+            int per = 1;
+            Voices.Tts(content, $"{path}{filename}", type, per);
             Console.ReadLine();
+
+            //string path = @"F:\";
+            //string url = "http://www.wujixiong.com";
+            //string name1 = "wujixiong";
+            //string name2 = "bigbeerqr";
+            //string name3 = "bigbeerzx";
+            //string BarCode = "20180123";//条形码数字（双数，80个以内）
+            //ImgType type = ImgType.Png;
+            //var sufiix = Suffix.Getsuffix(type);
+            //string logo = @"F:\123.jpg";//Logo地址 需要修改
+            //QrCode_QR.Generate(url,name1,path,type);//普通二维码
+            //Console.WriteLine($"文件保存成功，请在路径{path}查看文件{name1}{sufiix}");
+            //QRCode_Zxing.Generate(url,logo,path,name2,type,800,800);//带logo二维码 并且自定义大小
+            //Console.WriteLine($"文件保存成功，请在路径{path}查看文件{name2}{sufiix}");
+            //QRCode_Zxing.Generate2(BarCode,path,name3,type,300,100);//宽高比例3:1
+            //Console.WriteLine($"文件保存成功，请在路径{path}查看文件{name3}{sufiix}");
+            ////读取解析二维码和条形码
+            //var result1 =  QRCode_Zxing.Read($"{path}{name1}{sufiix}");
+            //var result2 = QRCode_Zxing.Read($"{path}{name2}{sufiix}");
+            //var result3 = QRCode_Zxing.Read($"{path}{name3}{sufiix}");
+            //Console.WriteLine(result1);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(result3);
+
+            //Console.ReadLine();
         }
     }
 }
